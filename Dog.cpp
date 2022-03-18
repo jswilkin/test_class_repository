3ef142b91ab58b9948576601756c1415a41c389a
jswilkin <jscottwilkin@gmail.com>
2022-03-17 17:53:23 -0700

    committing to git_demo

---
+++ Dog.cpp
@@ -0,0 +1,12 @@
+//Jacob Wilkin
+// Created by jswilkin on 3/17/22.
+//Dog running dog.h
+
+#include "Dog.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "Always dog" << endl;
+}
